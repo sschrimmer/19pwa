4875f2e0302faf40240b08f9b247885bd4071d22
Steven <stevenschrimmer1@gmail.com>
2023-08-09 21:10:10 -0500

    src folder with js and css

---
+++ client/src/js/database.js
@@ -0,0 +1,46 @@
+import { openDB } from "idb";
+
+const initdb = async () =>
+  openDB("jate", 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
+        return;
+      }
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  // Database and version
+  const jateDB = await openDB("jate", 1);
+  // New transaction specifying db and privileges
+  const tx = jateDB.transaction("jate", "readwrite");
+  // Open desired object store
+  const store = tx.objectStore("jate");
+  // Pass in content
+  const request = store.put({ id: 1, value: content });
+  // Confirmation
+  const result = await request;
+  console.log("🚀 - data saved to the database", result);
+};
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  // Database and version
+  const jateDB = await openDB("jate", 1);
+  // New transaction specifying db and privileges
+  const tx = jateDB.transaction("jate", "readonly");
+  // Open desired object store
+  const store = tx.objectStore("jate");
+  // Get all request
+  const request = store.getAll();
+  // Confirmation and return
+  const result = await request;
+  console.log("🚀 - data read from database", result);
+  return result.value;
+};
+
+initdb();
